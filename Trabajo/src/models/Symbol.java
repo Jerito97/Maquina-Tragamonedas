65498cf244f1bca7ef5b9635147c04e0e5cedda7
Tobias Ruano <ruano.t10@gmail.com>
2020-07-01 18:15:08 -0300

    test

--- Trabajo/src/models/Symbol.java
+++ Trabajo/src/models/Symbol.java
@@ -5,24 +5,18 @@ public class Symbol implements ISymbol {
 	int value;
 	String imgPath;
 
-//BUSCA LA IMAGEN
 	@Override
 	public String getImage() {
-		// TODO Auto-generated method stub
 		return imgPath;
 	}
 
-//PONE UN VALOR A LA FRUTA
 	@Override
 	public void setValue(int v) {
-		// TODO Auto-generated method stub
 		value = v;
 	}
 
-//DEVUELVE EL VALOR QUE SE LE DIO A LA FRUTA
 	@Override
 	public int getValue() {
-		// TODO Auto-generated method stub
 		return value;
 	}
 }
--- Trabajo/src/views/VentanaSaldo.java
+++ Trabajo/src/views/VentanaSaldo.java
@@ -14,34 +14,34 @@ public class VentanaSaldo extends JFrame {
 	JButton botonOk;
     JLabel labelSaldo;
     JTextField textSaldo;
-    
+
     public VentanaSaldo () {
     	configurar();
     	eventos();
     }
-    
+
     private void configurar() {
     	Container contenedor = this.getContentPane();
     	contenedor.setLayout(null);
     	this.setSize(300, 150);
     	this.setResizable(false);
-    	
+
     	labelSaldo = new JLabel("Ingrese saldo");
     	botonOk = new JButton("Ok");
     	textSaldo = new JTextField();
-    	
+
     	labelSaldo.setBounds(43, 20, 100, 30);
     	botonOk.setBounds(120, 80, 50, 30);
     	textSaldo.setBounds(147, 20, 100, 30);
-    	
+
     	contenedor.add(labelSaldo);
     	contenedor.add(botonOk);
     	contenedor.add(textSaldo);
     }
-    
+
     private void eventos() {
     	botonOk.addActionListener(new ActionListener() {
-    		
+
     		@Override
     		public void actionPerformed(ActionEvent e) {
     			String text = textSaldo.getText();
